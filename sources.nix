94085eef0f9ec8406f8240a7ca74d78aad48685e
Christian Foidl <christian@wunderwerk.io>
2024-01-13 15:55:58 +0100

    add arm hash

--- sources.nix
+++ sources.nix
@@ -8,7 +8,7 @@
   };
   aarch64-linux = fetchurl {
     url = "https://github.com/root-gg/plik/releases/download/${version}/plik-${version}-linux-arm64.tar.gz";
-    hash = "";
+    hash = "sha256-mW+7rGcYBC6aZW/7KwKWPnlmMHwe+enk5f6r+m0jZng=";
   };
 }
 
